a4f693e9db2ff4a2eb78eded41e517cf399931a0
natacha <nat_bartu@hotmail.com>
2022-10-12 14:14:35 +1000

    add function for random images

--- src/script.js
+++ src/script.js
@@ -6,5 +6,14 @@ const rows = 10;
 
 for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement("img");
-  img.src = `${unsplashUrl}`;
+  img.src = `${unsplashUrl}${getRandomSize()}`;
+  container.appendChild(img);
+}
+
+function getRandomSize() {
+  return `${getRandomNumber()}x${getRandomNumber()}`;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 300;
 }
